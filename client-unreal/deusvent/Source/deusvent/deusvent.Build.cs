de641e3b2d9addfe121367e65fdeeb98eb3621b5
Artem Yarulin <artem.yarulin@kapteko.com>
2024-08-12 17:29:10 +0300

    client-unreal Add Storage based on sqlite3

--- client-unreal/deusvent/Source/deusvent/deusvent.Build.cs
+++ client-unreal/deusvent/Source/deusvent/deusvent.Build.cs
@@ -1,5 +1,3 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 using UnrealBuildTool;
 using System.IO;
 
@@ -8,14 +6,35 @@ public class deusvent : ModuleRules
 	public deusvent(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+		});
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Logic lib
 		CppStandard = CppStandardVersion.Cpp20;
 		PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "../../ThirdParty/liblogic.a"));
 
+		// We assume sqlite3 is available everywhere
+		// TODO Paths looks very specific to my machine, let's check if those are the same on CI once we have it
+		if (Target.Platform == UnrealTargetPlatform.IOS)
+		{
+			string SDKPath = Path.Combine("/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/");
+			PublicAdditionalLibraries.Add(Path.Combine(SDKPath, "libsqlite3.tbd"));
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			string SDKPath = Path.Combine("/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/");
+			PublicAdditionalLibraries.Add(Path.Combine(SDKPath, "libsqlite3.tbd"));
+		}
+		else
+		{
+			PublicAdditionalLibraries.Add("sqlite3");
+		}
 	}
 }
+
