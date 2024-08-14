6ab022804a2452a2e01ce63d894c40047f045b46
Artem Yarulin <artem.yarulin@kapteko.com>
2024-08-14 16:22:31 +0300

    api Remove test from lambda-health because of outdated fixture

--- api/lambda-health/src/main.rs
+++ api/lambda-health/src/main.rs
@@ -12,22 +12,3 @@ async fn handler(_: Request) -> Result<Response<String>, Error> {
 async fn main() -> Result<(), Error> {
     run(service_fn(handler)).await
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::handler;
-    use lambda_http::{http::Request, Body};
-
-    fn req(body: &str) -> Request<Body> {
-        // TODO Fixture is wrong
-        let fixture = r#"{"requestContext":{"http":{"method":"GET"}},"body":"[BODY]"}"#;
-        let req = fixture.replace("[BODY]", body);
-        lambda_http::request::from_str(&req).unwrap()
-    }
-
-    #[tokio::test]
-    async fn test_handler() {
-        let response = handler(req("")).await.unwrap();
-        assert_eq!(response.status(), 200);
-    }
-}
