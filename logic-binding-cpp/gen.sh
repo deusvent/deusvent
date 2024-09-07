7d3ecb5b3fbc1286b29390a25fc3d84d50a61063
Artem Yarulin <artem.yarulin@kapteko.com>
2024-09-07 23:49:01 +0300

    ci Ensure directory for logic libraries exists

--- logic-binding-cpp/gen.sh
+++ logic-binding-cpp/gen.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
 # Build logic for all needed platforms
 cargo build --release --target=x86_64-unknown-linux-gnu \
                       --target=aarch64-apple-ios \
                       --target=aarch64-apple-darwin \
                       --target=aarch64-linux-android
+
+mkdir -p ../client-unreal/deusvent/ThirdParty # Ensure directory exists
 
 cp ../target/x86_64-unknown-linux-gnu/release/liblogic.a ../client-unreal/deusvent/ThirdParty/liblogic.amd64.linux.a
 cp ../target/aarch64-apple-ios/release/liblogic.a ../client-unreal/deusvent/ThirdParty/liblogic.arm64.ios.a
